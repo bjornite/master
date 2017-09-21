0d0c562d168443f280397b77686f5f695708ddb3
bjornite <bjornite@student.matnat.uio.no>
2017-09-21 12:25:59 +0200

    First attempt at knowledge based Q-learner for cartpole

--- code/basic_q_learning.py
+++ code/basic_q_learning.py
@@ -53,6 +53,58 @@ class Qlearner(Agent):
         else:
             return self.action_space.sample()
 
+class KBQlearner(Agent):
+    def __init__(self, name, env):
+        super(Qlearner, self).__init__(name, env)
+        print(env.reward_range)
+        self.model = KBTfTwoLayerNet(self.observation_space.shape[0],
+                                   self.action_space.n)
+        self.gamma = 0.9
+        self.tau = 0.01
+        self.random_action_prob = 0.5
+        self.random_action_decay = 0.999
+        self.observations = []
+        self.actions = []
+        self.replay_memory = []
+        self.replay_memory_size = 50000
+        self.minibatch_size = 100
+        self.old_weights = self.model.get_weights()
+        self.target_update_freq = 1000
+
+    def train(self):
+        data = random.sample(self.replay_memory, self.minibatch_size)
+        states = [m[0] for m in data]
+        a = [m[1] for m in data]
+        obs = [m[2] for m in data]
+        r = [m[3] for m in data]
+        targets = np.zeros(len(data))
+        targetActionMask = np.zeros(
+                        (self.minibatch_size, self.action_space.n), dtype=int)
+        target_actions = self.model.predict(obs)
+        target_q_values = self.model.predict(obs, weights=self.old_weights)
+        baseline_q_values = self.model.predict(states, weights=self.old_weights)
+        max_q_values = [target_q_values[i][np.argmax(target_actions[i])] for i in range(len(target_q_values))]
+        base_q_values = [baseline_q_values[i][np.argmax(target_actions[i])] for i in range(len(baseline_q_values))]
+        knowledge_rewards = self.model.get_prediction_error(states, obs)
+        max_knowledge_reward = np.max(knowledge_rewards)
+        for i in range(len(data)):
+            targets[i] = r[i]
+            if not data[i][4]:
+                targets[i] += (self.gamma*max_q_values[i] - base_q_values[i] + knowledge_rewards[i]/max_knowledge_reward)
+            targetActionMask[i][a[i]] = 1
+        self.model.train(states,
+                         obs,
+                         targets,
+                         targetActionMask)
+        self.random_action_prob *= self.random_action_decay
+
+    def get_action(self, observation):
+        values = self.model.predict([observation])
+        if random.random() > self.random_action_prob:
+            return np.argmax(values)
+        else:
+            return self.action_space.sample()
+
 class Random_agent(Agent):
     def get_action(self, input):
         return self.action_space.sample()
--- code/tf_neural_net.py
+++ code/tf_neural_net.py
@@ -84,3 +84,107 @@ class TfTwoLayerNet(object):
 
     def get_weights(self):
         return self.sess.run(self.weights)
+
+
+    class KBTfTwoLayerNet(object):
+    def __init__(self, input_size, output_size, summaries_dir="test_logs"):
+        self.learning_rate = 5e-5
+        self.n_input = input_size
+        self.n_hidden_1 = 128
+        self.n_hidden_2 = 128
+        self.n_output = int(output_size)
+        # TF model variables:
+        self.X = tf.placeholder("float", [None, self.n_input])
+        self.X_next = tf.placeholder("float", [None, self.n_input])
+        self.Q = tf.placeholder("float", [None, self.n_output])
+        self.beta = 1e-5
+        with tf.name_scope('layer_1'):
+            W1 = tf.Variable(
+                tf.random_normal([self.n_input, self.n_hidden_1],
+                                 stddev=np.sqrt(2.0 / self.n_input)), name='W1')
+            b1 = tf.Variable(tf.constant(0.1, shape=[self.n_hidden_1]), name='b1')
+            h1 = tf.nn.relu(tf.add(tf.matmul(self.X, W1), b1))
+        with tf.name_scope('layer_2'):
+            W2 = tf.Variable(
+                tf.random_normal([self.n_hidden_1, self.n_hidden_2],
+                                 stddev=np.sqrt(2.0 / self.n_hidden_1)), name='W2')
+            b2 = tf.Variable(tf.constant(0.1, shape=[self.n_hidden_2]), name='b2'),
+            h2 = tf.nn.relu(tf.add(tf.matmul(h1, W2), b2))
+        with tf.name_scope('output_layer'):
+            W3 = tf.Variable(
+                tf.random_normal([self.n_hidden_2, self.n_output],
+                                 stddev=np.sqrt(2.0 / self.n_hidden_2)), name='W3')
+            b3 = tf.Variable(tf.constant(0.1, shape=[self.n_output]), name='b3')
+            self.Q = tf.add(tf.matmul(h2, W3), b3)
+        with tf.name_scope('prediction_layer'):
+            WP = tf.Variable(
+                tf.random_normal([self.n_input, self.n_input],
+                                 stddev=np.sqrt(2.0 / self.n_input)), name='WP')
+            bP = tf.Variable(tf.constant(0.1, shape=[self.n_input]), name='bP')
+            self.prediction = tf.add(tf.matmul(self.X, WP), bP)
+
+        self.weights = [W1, b1, W2, b2, W3, b3]
+
+        # Prediction loss
+        self.pred_loss = (tf.reduce_mean(tf.square(tf.subtract(self.prediction, self.X_next))))
+
+        # Loss
+        self.targetQ = tf.placeholder(tf.float32, [None])
+        self.targetActionMask = tf.placeholder(
+            tf.float32, [None, self.n_output])
+
+        q_values = tf.reduce_sum(tf.multiply(self.Q, self.targetActionMask),
+                                 reduction_indices=[1])
+        self.loss = (tf.reduce_mean(tf.square(tf.subtract(q_values, self.targetQ))) +
+                     self.beta * tf.reduce_sum(tf.square(W1)) +
+                     self.beta * tf.reduce_sum(tf.square(W2)) +
+                     self.beta * tf.reduce_sum(tf.square(W3)) +
+                     self.beta * tf.reduce_sum(tf.square(WP)) +
+                     self.pred_loss)
+        tf.summary.scalar('loss', self.loss)
+
+
+        optimizer = tf.train.GradientDescentOptimizer(self.learning_rate)
+        self.global_step = tf.Variable(0, name='global_step', trainable=False)
+        self.train_op = optimizer.minimize(self.loss, global_step=self.global_step)
+
+        self.merged = tf.summary.merge_all()
+
+        init = tf.global_variables_initializer()
+        self.sess = tf.Session()
+        self.train_writer = tf.summary.FileWriter(summaries_dir + '/train',
+                                                  self.sess.graph)
+        self.test_writer = tf.summary.FileWriter(summaries_dir + '/test')
+        self.sess.run(init)
+
+    def predict(self, x, weights=None):
+        # Calculate prediction and ecoding
+        if weights is None:
+            q_values = self.sess.run(self.Q, feed_dict={self.X: x})
+        else:
+            feed_dict = {self.X: x}
+            feed_dict.update(zip(self.weights, weights))
+            q_values = self.sess.run(self.Q, feed_dict=feed_dict)
+        return q_values
+
+    def train(self, x, x_next, targetQ, targetActionMask):
+        # Calculate next prediction, the modules encoding and the error of the last prediction
+        # TODO: Optimize so the train step doesn't need to do a forward pass
+        # Calculate next prediction, the modules encoding and the error of the last prediction
+        feed_dict = {self.X: x,
+                     self.X_next: x_next,
+                     self.targetQ: targetQ,
+                     self.targetActionMask: targetActionMask}
+        opt, summary, global_step = self.sess.run([self.train_op, self.merged, self.global_step],
+                                                  feed_dict=feed_dict)
+        self.train_writer.add_summary(summary, global_step)
+        return
+
+    def get_prediction_error(self, x, x_next):
+        feed_dict = {self.X: x,
+                     self.X_next: x_next}
+        return self.sess.run([self.pred_loss],
+                             feed_dict=feed_dict)
+
+    def get_weights(self):
+        return self.sess.run(self.weights)
