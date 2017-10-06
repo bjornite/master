35a4fb6af92a240787df129b1f20adeb5c6f78e9
bjornite <bjornite@student.matnat.uio.no>
2017-10-06 13:19:47 +0200

    parallellised testing

--- run_tests.py
+++ run_tests.py
@@ -1,4 +1,7 @@
 import os
+from functools import partial
+from multiprocessing.dummy import Pool
+from subprocess import call
 
 DEFAULT_NUM_RUNS = 15
 RUN_FILE = "framework.py"
@@ -9,14 +12,16 @@ agents = ["Qlearner",
 env = "CartPole-v1"
 DEFAULT_NUM_ROLLOUTS = 2000
 
+commands = []
+
 log_dir = "log_archive/2000_Q-KBQ-CBQ_CartPole-v1"
 try:
     os.mkdir(log_dir)
 except:
     pass
-for agent in agents:
-    for i in range(DEFAULT_NUM_RUNS):
-        os.system(
+for i in range(DEFAULT_NUM_RUNS):
+    for agent in agents:
+        commands.append(
             "python {0} {1} {2} --log_dir_root={3} --num_rollouts={4}".format(
                 RUN_FILE,
                 agent,
@@ -29,12 +34,17 @@ try:
     os.mkdir(log_dir)
 except:
     pass
-for agent in agents:
-    for i in range(DEFAULT_NUM_RUNS):
-        os.system(
+for i in range(DEFAULT_NUM_RUNS):
+    for agent in agents:
+        commands.append(
             "python {0} {1} {2} --log_dir_root={3} --num_rollouts={4} --random_cartpole".format(
                 RUN_FILE,
                 agent,
                 env,
                 log_dir,
                 DEFAULT_NUM_ROLLOUTS))
+
+pool = Pool(8) # two concurrent commands at a time
+for i, returncode in enumerate(pool.imap(partial(call, shell=True), commands)):
+    if returncode != 0:
+       print("%d command failed: %d" % (i, returncode))
