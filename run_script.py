4d770df53837073d91f6efcf80243348b8f67a9c
bjornite <bjornite@student.matnat.uio.no>
2017-10-10 14:43:07 +0200

    paralellised run_script

--- run_script.py
+++ run_script.py
@@ -1,6 +1,9 @@
 import os
 import argparse
 import datetime
+from functools import partial
+from multiprocessing.dummy import Pool
+from subprocess import call
 from utilities import get_time_string, get_log_dir, parse_time_string
 
 DEFAULT_NUM_RUNS = 15
@@ -9,6 +12,7 @@ LOG_DIR_ROOT = "logfiles"
 DEFAULT_AGENT = "Qlearner"
 DEFAULT_ENV = "CartPole-v1"
 DEFAULT_NUM_ROLLOUTS = 500
+DEFAULT_NUM_WORKERS = 7
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--num_runs', type=int, default=DEFAULT_NUM_RUNS)
@@ -26,12 +30,21 @@ log_tf = "--no_tf_log"
 if args.log_tf:
     log_tf = ""
 
+commands = []
+
 for i in range(args.num_runs):
-    os.system("python {0} {1} {2} --log_dir_root={3} --num_rollouts={4} {5}".format(RUN_FILE,
-                                                                                    args.agentname,
-                                                                                    args.envname,
-                                                                                    LOG_DIR_ROOT,
-                                                                                    args.num_rollouts,
-                                                                                    log_tf))
+    commands.append(
+        "python {0} {1} {2} --log_dir_root={3} --num_rollouts={4} {5}".format(
+            RUN_FILE,
+            args.agentname,
+            args.envname,
+            LOG_DIR_ROOT,
+            args.num_rollouts,
+            log_tf))
+
+pool = Pool(args.num_workers)  # two concurrent commands at a time
+for i, returncode in enumerate(pool.imap(partial(call, shell=True), commands)):
+    if returncode != 0:
+        print("%d command failed: %d" % (i, returncode))
 
 print("Success")
