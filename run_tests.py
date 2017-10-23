6577d0148b4bd57e41045a0490542cf842727cc4
Bjørn Ivar <bjornivarteigen@gmail.com>
2017-10-23 21:32:02 +0200

    Testing agents on lunarlander

--- run_tests.py
+++ run_tests.py
@@ -7,19 +7,19 @@ from subprocess import call
 from utilities import get_time_string, get_log_dir, parse_time_string
 
 
-DEFAULT_NUM_RUNS = 5
+DEFAULT_NUM_RUNS = 1
 RUN_FILE = "framework.py"
 LOG_DIR_ROOT = "tests"
 agents = [
-    #"Qlearner",
-    #"KBQlearner",
-    #"IKBQlearner",
-    #"CBQlearner",
+    "Qlearner",
+    "KBQlearner",
+    "IKBQlearner",
+    "CBQlearner",
     "SAQlearner",
     "ISAQlearner",
     "MSAQlearner",
     "IMSAQlearner"]
-env = "CartPole-v1"
+env = "LunarLander-v2"
 DEFAULT_NUM_ROLLOUTS = 1000
 DEFAULT_NUM_WORKERS = 1
 
@@ -39,7 +39,7 @@ start_datetime = parse_time_string(start_time)
 
 commands = []
 
-log_dir = "standard_SAQ"
+log_dir = "lander_SAQ"
 try:
     os.mkdir(log_dir)
 except:
@@ -55,23 +55,23 @@ for i in range(args.num_runs):
                 args.num_rollouts,
                 log_tf))
 
-log_dir = "random_SAQ"
-try:
-    os.mkdir(log_dir)
-except:
-    pass
-for i in range(args.num_runs):
-    for agent in agents:
-        commands.append(
-            "python {0} {1} {2} --log_dir_root={3} --num_rollouts={4} --random_cartpole {5}".format(
-                RUN_FILE,
-                agent,
-                env,
-                log_dir,
-                args.num_rollouts,
-                log_tf))
+# log_dir = "random_SAQ"
+# try:
+#     os.mkdir(log_dir)
+# except:
+#     pass
+# for i in range(args.num_runs):
+#     for agent in agents:
+#         commands.append(
+#             "python {0} {1} {2} --log_dir_root={3} --num_rollouts={4} --random_cartpole {5}".format(
+#                 RUN_FILE,
+#                 agent,
+#                 env,
+#                 log_dir,
+#                 args.num_rollouts,
+#                 log_tf))
 
-pool = Pool(args.num_workers)  # two concurrent commands at a time
-for i, returncode in enumerate(pool.imap(partial(call, shell=True), commands)):
-    if returncode != 0:
-        print("%d command failed: %d" % (i, returncode))
+# pool = Pool(args.num_workers)  # two concurrent commands at a time
+# for i, returncode in enumerate(pool.imap(partial(call, shell=True), commands)):
+#     if returncode != 0:
+#         print("%d command failed: %d" % (i, returncode))
