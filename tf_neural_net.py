15b73ac96dd1940c68e3199194d62c1610bfe1eb
Bjørn Ivar <bjornivarteigen@gmail.com>
2017-10-24 13:40:50 +0200

    Limited GPU memory allocation to each thread

--- tf_neural_net.py
+++ tf_neural_net.py
@@ -161,6 +161,7 @@ class CBTfTwoLayerNet(object):
         config = tf.ConfigProto(
              device_count={'GPU': 1}
         )
+        config.gpu_options.per_process_gpu_memory_fraction = 0.4
         self.sess = tf.Session(config=config)
         self.train_writer = tf.summary.FileWriter(log_dir,
                                                   self.sess.graph)
